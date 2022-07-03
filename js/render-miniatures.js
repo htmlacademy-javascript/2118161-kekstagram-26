4bff19f2952212e2c0de096f2441bdf345915192
Ekaterina Gladkova <kirova94@mail.ru>
2022-07-04 00:17:37 +0300

    Добавила модуль для отрисовки миниатюр

---
+++ js/render-miniatures.js
@@ -0,0 +1,20 @@
+import {createPublications} from './data.js';
+
+const otherUsersPhotosContainer = document.querySelector('.pictures');
+const photoTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+
+const similarPhotos = createPublications();
+
+const similarPhotosFragment = document.createDocumentFragment();
+
+similarPhotos.forEach(({url, likes, comments}) => {
+  const otherUserPhoto = photoTemplate.cloneNode(true);
+  otherUserPhoto.querySelector('.picture__img').src = url;
+  otherUserPhoto.querySelector('.picture__likes').textContent = likes;
+  otherUserPhoto.querySelector('.picture__comments').textContent = comments;
+  similarPhotosFragment.appendChild(otherUserPhoto);
+});
+
+otherUsersPhotosContainer.appendChild(similarPhotosFragment);
