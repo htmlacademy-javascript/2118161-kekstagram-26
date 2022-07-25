5af3b7e0980e8bf454e193b4082d47f934cc3125
Keks <keks@htmlacademy.ru>
2022-07-25 23:10:04 +0300

    :heavy_check_mark: Сборка #12

---
+++ 12/js/edit-photo.js
@@ -0,0 +1,166 @@
+const MAX_ZOOM_VALUE = 100;
+const MIN_ZOOM_VALUE = 25;
+const STEP_ZOOM_VALUE = 25;
+const DEFAULT_ZOOM_VALUE = 100;
+const EFFECTS_OPTIONS = {
+  none: {
+    filterName: '',
+    measureUnit: '',
+    range: {
+      min: 0,
+      max: 0,
+    },
+    start: 0,
+    step: 0,
+  },
+  chrome: {
+    filterName: 'grayscale',
+    measureUnit: '',
+    range: {
+      min: 0,
+      max: 1,
+    },
+    start: 1,
+    step: 0.1,
+  },
+  sepia: {
+    filterName: 'sepia',
+    measureUnit: '',
+    range: {
+      min: 0,
+      max: 1,
+    },
+    start: 1,
+    step: 0.1,
+  },
+  marvin: {
+    filterName: 'invert',
+    measureUnit: '%',
+    range: {
+      min: 0,
+      max: 100,
+    },
+    start: 100,
+    step: 1,
+  },
+  phobos: {
+    filterName: 'blur',
+    measureUnit: 'px',
+    range: {
+      min: 0,
+      max: 3,
+    },
+    start: 3,
+    step: 0.1,
+  },
+  heat: {
+    filterName: 'brightness',
+    measureUnit: '',
+    range: {
+      min: 1,
+      max: 3,
+    },
+    start: 3,
+    step: 0.1,
+  },
+};
+
+const photoPreview = document.querySelector('.img-upload__preview  img');
+
+const zoomOutButton = document.querySelector('.scale__control--smaller');
+const zoomInButton = document.querySelector('.scale__control--bigger');
+const zoomScaleLabel = document.querySelector('.scale__control--value');
+
+const effectsFieldsetNode = document.querySelector('.img-upload__effects');
+const effectsLevelSlider = document.querySelector('.effect-level__slider');
+const effectsLevelValue = document.querySelector('.effect-level__value');
+
+let scale;
+let selectedEffect;
+
+const resetEdits = () => {
+  document.querySelector('#effect-none').checked = true;
+  scale = DEFAULT_ZOOM_VALUE;
+  selectedEffect = 'none';
+  zoomScaleLabel.value = '100 %';
+  photoPreview.style.transform = 'scale(1)';
+  photoPreview.className = '';
+  photoPreview.classList.add('effects__preview--none');
+  photoPreview.style.filter = '';
+  effectsLevelSlider.style.visibility = 'hidden';
+};
+
+resetEdits();
+
+const renderScalingPhoto = (evt) => {
+  if (evt.target.matches('.scale__control--smaller') && scale > MIN_ZOOM_VALUE) {
+    scale -= STEP_ZOOM_VALUE;
+  }
+  if (evt.target.matches('.scale__control--bigger') && scale < MAX_ZOOM_VALUE) {
+    scale += STEP_ZOOM_VALUE;
+  }
+  zoomScaleLabel.value = `${scale} %`;
+
+  const scaleDecimal = scale / 100;
+  photoPreview.style.transform = `scale(${scaleDecimal})`;
+};
+
+const onZoomButtonClick = (evt) => {
+  renderScalingPhoto(evt);
+};
+
+noUiSlider.create(effectsLevelSlider, {
+  range: {
+    min: 0,
+    max: 100,
+  },
+  start: 100,
+  step: 1,
+  connect: 'lower',
+});
+
+const onNoUiSliderUpdate = () => {
+  const selectedFilterType = EFFECTS_OPTIONS[selectedEffect].filterName;
+  const selectedFilterMeasureUnit = EFFECTS_OPTIONS[selectedEffect].measureUnit;
+  effectsLevelValue.value = effectsLevelSlider.noUiSlider.get();
+  photoPreview.style.filter = `${selectedFilterType}(${effectsLevelValue.value}${selectedFilterMeasureUnit})`;
+};
+
+effectsLevelSlider.noUiSlider.on('update', onNoUiSliderUpdate);
+
+const renderPhotoEffect = (evt) => {
+  const effectName = evt.target.value;
+  photoPreview.className = '';
+  photoPreview.classList.add(`effects__preview--${effectName}`);
+  if (effectName === 'none') {
+    effectsLevelSlider.style.visibility = 'hidden';
+    photoPreview.style.filter = '';
+  } else {
+    effectsLevelSlider.style.visibility = 'visible';
+    selectedEffect = effectName;
+    effectsLevelSlider.noUiSlider.updateOptions(EFFECTS_OPTIONS[effectName]);
+  }
+};
+
+const onEffectsFieldsetNode = (evt) => {
+  const clickedEffectRadio = evt.target.closest('.effects__radio');
+  if (clickedEffectRadio) {
+    renderPhotoEffect(evt);
+  }
+};
+
+const loadEditPhotoFuncs = () => {
+  resetEdits();
+  zoomOutButton.addEventListener('click', onZoomButtonClick);
+  zoomInButton.addEventListener('click', onZoomButtonClick);
+  effectsFieldsetNode.addEventListener('change', onEffectsFieldsetNode);
+};
+
+const unloadEditPhotoFuncs = () => {
+  resetEdits();
+  zoomOutButton.removeEventListener('click', onZoomButtonClick);
+  zoomInButton.removeEventListener('click', onZoomButtonClick);
+  effectsFieldsetNode.removeEventListener('change', onEffectsFieldsetNode);
+};
+
+export {resetEdits, loadEditPhotoFuncs, unloadEditPhotoFuncs};
---
+++ 12/js/load-photo-form.js
@@ -0,0 +1,179 @@
+import {isStringSatisfyMaxLength, isEscapeKey, openErrorLoadMessage, openSuccessLoadMessage} from './util.js';
+import {resetEdits, loadEditPhotoFuncs, unloadEditPhotoFuncs} from './edit-photo.js';
+import {sendData} from './api.js';
+
+const MAX_HASHTAGS_COUNT = 5;
+const MAX_DESCRIPTION_SYMBOLS_COUNT = 140;
+const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+
+const uploadFileForm = document.querySelector('.img-upload__form');
+const uploadFileNode = uploadFileForm.querySelector('#upload-file');
+const uploadPreview = uploadFileForm.querySelector('.img-upload__preview > img');
+const photoEditNode = uploadFileForm.querySelector('.img-upload__overlay');
+const hashtagsInputNode = uploadFileForm.querySelector('.text__hashtags');
+const descriptionInputNode = uploadFileForm.querySelector('.text__description');
+const cancelButton = photoEditNode.querySelector('#upload-cancel');
+const submitButton = photoEditNode.querySelector('#upload-submit');
+
+const pristine = new Pristine(uploadFileForm, {
+  classTo: 'img-upload__field-wrapper',
+  errorClass: 'img-upload__field-wrapper_invalid',
+  successClass: 'img-upload__field-wrapper_valid',
+  errorTextParent:'img-upload__field-wrapper',
+  errorTextTag: 'p',
+  errorTextClass: 'input__error'
+});
+const re = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/;
+
+const removeHashtagsExcessSpace = (value) => {
+  const hashtagsArray = value.split(' ');
+  const hashtags = hashtagsArray.filter((hashtag) => hashtag !== '');
+  return hashtags;
+};
+
+const validateHashtagsContent = (value) => {
+  const hashtags = removeHashtagsExcessSpace(value);
+  for (let i = 0; i < hashtags.length; i++) {
+    if (!re.test(hashtags[i])) {
+      return false;
+    }
+  }
+  return true;
+};
+const validateHashtagsContentErrorMessage = 'Хэш-тег должен начинаться с символа # (решётка) <br> и состоять из букв и чисел, <br> максимальная длина одного хэш-тега 20 символов, включая решётку';
+
+const validateHashtagsQuantity = (value) => {
+  const hashtags = removeHashtagsExcessSpace(value);
+  if (hashtags.length > MAX_HASHTAGS_COUNT) {
+    return false;
+  }
+  return true;
+};
+const validateHashtagsQuantityErrorMessage = 'Нельзя указать больше пяти хэш-тегов';
+
+const validateHashtagsRepeats = (value) => {
+  const hashtags = removeHashtagsExcessSpace(value);
+  for (let i = 0; i < hashtags.length; i++) {
+    for (let j = i + 1; j < hashtags.length; j++) {
+      if (hashtags[i].toLowerCase() === hashtags[j].toLowerCase()) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+const validateHashtagsRepeatsErrorMessage = 'Один и тот же хэш-тег не может быть использован дважды';
+
+const validateDescriptionSymbolsQuantity  = (description) => {
+  const valid = isStringSatisfyMaxLength(description, MAX_DESCRIPTION_SYMBOLS_COUNT);
+  return valid;
+};
+const validateDescriptionSymbolsQuantityErrorMessage = 'Длина комментария не может составлять больше 140 символов';
+
+pristine.addValidator(hashtagsInputNode, validateHashtagsContent, validateHashtagsContentErrorMessage);
+pristine.addValidator(hashtagsInputNode, validateHashtagsQuantity, validateHashtagsQuantityErrorMessage);
+pristine.addValidator(hashtagsInputNode, validateHashtagsRepeats, validateHashtagsRepeatsErrorMessage);
+pristine.addValidator(descriptionInputNode, validateDescriptionSymbolsQuantity, validateDescriptionSymbolsQuantityErrorMessage);
+
+const onTextInputNodeKeydown = (evt) => {
+  evt.stopPropagation();
+};
+
+const resetTextFields = () => {
+  const errorTextNodes = uploadFileForm.querySelectorAll('.input__error');
+  if (errorTextNodes.length > 0) {
+    errorTextNodes.forEach((textNode) => {textNode.textContent = '';});
+  }
+
+  hashtagsInputNode.value = '';
+  descriptionInputNode.value = '';
+};
+
+const closeEditPhotoModal = () => {
+  document.body.classList.remove('modal-open');
+  photoEditNode.classList.add('hidden');
+
+  resetEdits();
+  resetTextFields();
+  unloadEditPhotoFuncs();
+
+  hashtagsInputNode.removeEventListener('keydown', onTextInputNodeKeydown);
+  descriptionInputNode.removeEventListener('keydown', onTextInputNodeKeydown);
+  uploadFileForm.removeEventListener('submit', onSubmitUploadFileForm);
+  cancelButton.removeEventListener('click', onCancelButtonClick);
+  document.removeEventListener('keydown', onDocumentKeydown);
+};
+
+function onCancelButtonClick () {
+  closeEditPhotoModal();
+}
+
+const openEditPhotoModal = () => {
+  document.body.classList.add('modal-open');
+  photoEditNode.classList.remove('hidden');
+
+  resetEdits();
+  loadEditPhotoFuncs();
+
+  hashtagsInputNode.addEventListener('keydown', onTextInputNodeKeydown);
+  descriptionInputNode.addEventListener('keydown', onTextInputNodeKeydown);
+  uploadFileForm.addEventListener('submit', onSubmitUploadFileForm);
+  cancelButton.addEventListener('click', onCancelButtonClick);
+  document.addEventListener('keydown', onDocumentKeydown);
+};
+
+function onDocumentKeydown (evt) {
+  const openedErrorLoadMessage = document.querySelector('.error');
+  const openedSuccessLoadMessage = document.querySelector('.success');
+  if (isEscapeKey(evt) && !openedErrorLoadMessage && !openedSuccessLoadMessage) {
+    evt.preventDefault();
+    closeEditPhotoModal();
+  }
+}
+
+const onUploadFileNodeChange = () => {
+  const file = uploadFileNode.files[0];
+  const fileName = file.name.toLowerCase();
+  const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
+
+  if (matches) {
+    uploadPreview.src = URL.createObjectURL(file);
+  }
+  openEditPhotoModal();
+};
+
+uploadFileNode.addEventListener('change', onUploadFileNodeChange);
+
+const onSuccessSubmit = () => {
+  unblockSubmitButton();
+  closeEditPhotoModal();
+  openSuccessLoadMessage();
+};
+
+const onErrorSubmit = () => {
+  unblockSubmitButton();
+  openErrorLoadMessage();
+};
+
+function onSubmitUploadFileForm (evt) {
+  const isValid = pristine.validate();
+  evt.preventDefault();
+  if (isValid) {
+    blockSubmitButton();
+    sendData(
+      () => onSuccessSubmit(),
+      () => onErrorSubmit(),
+      new FormData(evt.target),
+    );
+  }
+}
+
+function blockSubmitButton () {
+  submitButton.disabled = true;
+  submitButton.textContent = 'Публикация...';
+}
+
+function unblockSubmitButton () {
+  submitButton.disabled = false;
+  submitButton.textContent = 'Опубликовать';
+}
